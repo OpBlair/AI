772aff536651319247c1874735059c2fad4539df
Tonny Blair <jusanotherthursday@gmail.com>
2025-12-14 22:44:00 +0300

    Update app.py

--- Chess/backend/app.py
+++ Chess/backend/app.py
@@ -10,7 +10,7 @@ def home():
     return "Chess AI backend is running...."
 
 #--- API Route placeholder
-@app.route('/api/test_connection', methods['POST'])
+@app.route('/api/test_connection', methods=['POST'])
 def test_connection():
     #Receive full board state object from frontend.
     try:
@@ -19,9 +19,20 @@ def test_connection():
         current_player = data.get('currentPlayer')
 
         if not board_state:
-            return jsonify({"Status": "error", "message": "Missing boardState in request}), 400
+            return jsonify({
+            	"Status": "error", 
+            	"message": "Missing boardState in request"
+            }), 400
 
         print(f"\n--- Received Data from Frontend ---")
         print(f"Current Player: {current_player}")
         print(f"Board State Keys (Sample): {list(board_state.keys())[:4]}...")
         print(f"--- End Received Data --- \n")
+
+    except Exception as e:   
+        app.logger.error(f"Error in test_coonection: {e}")
+        return jsonify({"status": "error", "message": f"server error: {e}"}), 500
+
+if __name__ == '__main__':
+        print("Starting Flask Server on http://127.0.0.1:5000")
+        app.run(debug=True, port=5000)
